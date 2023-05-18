ae1d97b7437fa0412035a045033b867233d79374
khachung2004 <hungnkph35189@fpt.edu.vn>
2023-05-18 08:52:34 +0700

    updateData

---
+++ app/src/main/java/com/hungnk/baitapbuoi4/ContraintLayout2.java
@@ -0,0 +1,14 @@
+package com.hungnk.baitapbuoi4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContraintLayout2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contraint_layout2);
+    }
+}
---
+++ app/src/main/java/com/hungnk/baitapbuoi4/ContraintLayout3.java
@@ -0,0 +1,14 @@
+package com.hungnk.baitapbuoi4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContraintLayout3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contraint_layout3);
+    }
+}
